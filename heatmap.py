5940b9571e4d7ddc6e6322b576fb6096a197c6c4
Alexander Mot <alexander@amot.dev>
2024-03-02 11:04:54 -0700

    Add basic heatmap code

--- heatmap.py
+++ heatmap.py
@@ -1,12 +1,30 @@
 import tkinter as tk
 
-# placeholder
-heatmap = [["#FFF000" for _ in range(10)] for _ in range(10)]
 
+def view_heatmap(root):
+    heatmap_window = tk.Toplevel(root)
+    layer_toggle_labels = ["Oil", "Metals", "Helium", "Ships", "Coral Reef", "Endangered Species"]
 
-def view_map(root):
-    map_window = tk.Toplevel(root)
-    for i in range(10):
-        for j in range(10):
-            label = tk.Label(map_window, bg=heatmap[i][j], width=2, height=1)
-            label.grid(row=i, column=j)
+    # Create layer toggles
+    for i in range(len(layer_toggle_labels)):
+        toggle = tk.Checkbutton(heatmap_window, text=layer_toggle_labels[i], height=1)
+        toggle.grid(row=i, column=0, pady=0)
+
+    # Create canvas grid
+    canvas = tk.Canvas(heatmap_window, width=500, height=500)
+    canvas.grid(row=0, column=1, rowspan=100)
+    cell_size = 5  # size of one cell in pixels
+
+    # Create cells in grid
+    for i in range(100):
+        for j in range(100):
+            rectangle_left_edge = j * cell_size
+            rectangle_top_edge = i * cell_size
+            rectangle_right_edge = rectangle_left_edge + cell_size
+            rectangle_bottom_edge = rectangle_top_edge + cell_size
+            color = "#808080"
+            canvas.create_rectangle(rectangle_left_edge,
+                                    rectangle_top_edge,
+                                    rectangle_right_edge,
+                                    rectangle_bottom_edge,
+                                    fill=color, outline="")
